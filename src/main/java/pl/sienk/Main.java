6ae14e5a419a4e15288242cc05bb398cfd8f320c
macsienkiewicz <sienkiewiczmaciej00@gmail.com>
2023-01-05 11:13:05 +0100

    program running in loop

--- src/main/java/pl/sienk/Main.java
+++ src/main/java/pl/sienk/Main.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input number (higher or equal 0 and less than 2147483629): ");
-        int number = scan.nextInt();
-        System.out.println("Next prime number is: " + Algorithm.findNextPrime(number));
+        boolean stopRunning = false;
+        while(!stopRunning) {
+            try {
+                System.out.println("Input number (higher or equal 0 and less than 2147483629): ");
+                int number = scan.nextInt();
+                System.out.println("Next prime number is: " + Algorithm.findNextPrime(number));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("If you want to finish program press 'y' else press anything: ");
+            String stopChar = scan.next();
+            if(stopChar.equals("y")) {
+                stopRunning = true;
+            }
+        }
     }
 }
